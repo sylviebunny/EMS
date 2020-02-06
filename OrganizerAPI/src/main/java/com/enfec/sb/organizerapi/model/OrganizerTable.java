da88c1a3624e8832355b5bf6afb49439299517e9
Heidi Huo <heidihuo@Heidis-MacBook-Pro.local>
2020-02-05 19:29:39 -0800

    Add Lombok dependecy

--- OrganizerAPI/src/main/java/com/enfec/sb/organizerapi/model/OrganizerTable.java
+++ OrganizerAPI/src/main/java/com/enfec/sb/organizerapi/model/OrganizerTable.java
@@ -3,9 +3,13 @@ package com.enfec.sb.organizerapi.model;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 
 @Data
 @Component
+@Getter @Setter
 public class OrganizerTable {
 	
 	private int organizer_id;
@@ -13,45 +17,5 @@ public class OrganizerTable {
 	private String email_address; 
 	private String password; 
 	private String other_details; 
-	
-	public void setOrganizer_id(int orgId) {
-		organizer_id = orgId; 
-	}
-	
-	public int getOrganizer_id() {
-		return organizer_id; 
-	}
-	
-	public void setOrganizer_name(String orgName) {
-		organizer_name = orgName; 
-	}
-	
-	public String getOrganizer_name() {
-		return organizer_name; 
-	}
-	
-	public String getEmail_address() {
-		return email_address;
-	}
-
-	public void setEmail_address(String email_address) {
-		this.email_address = email_address;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getOther_details() {
-		return other_details;
-	}
-
-	public void setOther_details(String other_details) {
-		this.other_details = other_details;
-	}
 
 }

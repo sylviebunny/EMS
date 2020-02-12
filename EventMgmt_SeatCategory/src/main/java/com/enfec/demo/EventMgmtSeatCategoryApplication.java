8aa60cec1d645726ef09bf8e202fcc5b6b945d67
sylviebunny <35503172+sylviebunny@users.noreply.github.com>
2020-02-12 11:56:13 -0800

    SeatCategoryAPI, CRUD improvement

---
+++ EventMgmt_SeatCategory/src/main/java/com/enfec/demo/EventMgmtSeatCategoryApplication.java
@@ -0,0 +1,13 @@
+package com.enfec.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMgmtSeatCategoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventMgmtSeatCategoryApplication.class, args);
+	}
+
+}

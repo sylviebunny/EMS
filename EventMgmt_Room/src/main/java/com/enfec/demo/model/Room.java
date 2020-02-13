c6cdcf08d54f15e102b5b309c395426938e9fe21
sylviebunny <35503172+sylviebunny@users.noreply.github.com>
2020-02-12 16:28:12 -0800

    Merge RoomAPI to Master

---
+++ EventMgmt_Room/src/main/java/com/enfec/demo/model/Room.java
@@ -0,0 +1,36 @@
+package com.enfec.demo.model;
+
+import java.sql.Timestamp;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@Component
+public class Room {
+	public int Room_ID; 
+	public int Venue_ID; 
+	public String Room_Name; 
+	public int Room_Capability;
+	public double Rate_for_Day; 
+	public String Other_Details;
+	
+	public int Space_Request_ID; 
+	public int Event_ID;
+	public String Booking_Status_Code; 
+	public boolean Occupancy; 
+	public boolean Commercial_or_Free; 
+	public Timestamp Occupancy_Date_From;
+	public Timestamp Occupancy_Date_To;
+}

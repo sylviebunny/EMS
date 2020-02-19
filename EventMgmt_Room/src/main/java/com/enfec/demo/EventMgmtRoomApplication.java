b2244f5ae33bc0c626c7c637404c670dce9236a1
sylviebunny <35503172+sylviebunny@users.noreply.github.com>
2020-02-19 11:31:41 -0800

    Add dependency and initializer for deployment on ec2

--- EventMgmt_Room/src/main/java/com/enfec/demo/EventMgmtRoomApplication.java
+++ EventMgmt_Room/src/main/java/com/enfec/demo/EventMgmtRoomApplication.java
@@ -2,10 +2,17 @@ package com.enfec.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EventMgmtRoomApplication {
+public class EventMgmtRoomApplication extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EventMgmtRoomApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EventMgmtRoomApplication.class, args);
 	}

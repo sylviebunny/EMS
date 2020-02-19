abe22d92e6390780668992ea0bac0435136e2e69
sylviebunny <35503172+sylviebunny@users.noreply.github.com>
2020-02-19 12:36:01 -0800

    Update for deployment

--- EventMgmt_Organizer/src/main/java/com/enfec/demo/EventMgmtApplication.java
+++ EventMgmt_Organizer/src/main/java/com/enfec/demo/EventMgmtApplication.java
@@ -2,10 +2,18 @@ package com.enfec.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class EventMgmtApplication {
+public class EventMgmtApplication extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EventMgmtApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EventMgmtApplication.class, args);
 	}

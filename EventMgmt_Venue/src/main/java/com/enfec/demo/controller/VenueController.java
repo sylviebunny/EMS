9ff60f22b9feb4fd82a13c1411e72a7e75283904
sylviebunny <35503172+sylviebunny@users.noreply.github.com>
2020-02-19 11:28:09 -0800

    Add dependency and initializer for deployment on ec2

--- EventMgmt_Venue/src/main/java/com/enfec/demo/controller/VenueController.java
+++ EventMgmt_Venue/src/main/java/com/enfec/demo/controller/VenueController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.enfec.demo.exception.RecordNotFoundException;
 import com.enfec.demo.model.Venue;
 import com.enfec.demo.repository.VenueRepositoryImpl;
 import com.google.gson.Gson;

2739b7ecd5a63386906aca69442f90a662d2dbf4
Chad Chai <jinchao.chai@gmail.com>
2020-02-19 12:06:32 -0800

    Update pox.xml

--- CustomerAPI/src/main/java/com/enfec/EMS/CustomerAPI/CustomerApiApplication.java
+++ CustomerAPI/src/main/java/com/enfec/EMS/CustomerAPI/CustomerApiApplication.java
@@ -2,12 +2,21 @@ package com.enfec.EMS.CustomerAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CustomerApiApplication {
+public class CustomerApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApiApplication.class, args);
 	}
+	
+	 @Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		 return builder.sources(CustomerApiApplication.class);
+	 }
+
+
 
 }

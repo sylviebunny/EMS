44890539a906330e94f8d99327dc91f5fbd1b10b
Chad Chai <jinchao.chai@gmail.com>
2020-02-19 12:26:09 -0800

    update pom.xml

--- CustomerOrderAPI/src/main/java/com/enfec/EMS/CustomerOrderAPI/CustomerOrderApiApplication.java
+++ CustomerOrderAPI/src/main/java/com/enfec/EMS/CustomerOrderAPI/CustomerOrderApiApplication.java
@@ -2,12 +2,19 @@ package com.enfec.EMS.CustomerOrderAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CustomerOrderApiApplication {
+public class CustomerOrderApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerOrderApiApplication.class, args);
 	}
+	
+	 @Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		 return builder.sources(CustomerOrderApiApplication.class);
+	 }
 
 }

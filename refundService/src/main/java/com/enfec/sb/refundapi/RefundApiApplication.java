9f9ce1020a7bde0d6e934d704b6b7021ca0b81aa
Heidi Huo <heidihuo@Heidis-MacBook-Pro.local>
2020-02-19 16:34:16 -0800

    Deploy to AWS

--- refundService/src/main/java/com/enfec/sb/refundapi/RefundApiApplication.java
+++ refundService/src/main/java/com/enfec/sb/refundapi/RefundApiApplication.java
@@ -3,14 +3,20 @@ package com.enfec.sb.refundapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RefundApiApplication {
+public class RefundApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RefundApiApplication.class, args);
 		
 	}
 	
+   @Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+      return builder.sources(RefundApiApplication.class);
+   }
 
 }

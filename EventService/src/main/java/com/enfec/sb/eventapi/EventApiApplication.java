c39ed450bae77332c0dbca3bc39c67fbd4f9faf0
Heidi Huo <heidihuo@Heidis-MacBook-Pro.local>
2020-02-19 16:12:12 -0800

    Connect to aws

--- EventService/src/main/java/com/enfec/sb/eventapi/EventApiApplication.java
+++ EventService/src/main/java/com/enfec/sb/eventapi/EventApiApplication.java
@@ -3,14 +3,20 @@ package com.enfec.sb.eventapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EventApiApplication {
-
+public class EventApiApplication extends SpringBootServletInitializer{
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EventApiApplication.class, args);
 		
 	}
 	
+	@Override
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(EventApiApplication.class);
+   }
 
 }
